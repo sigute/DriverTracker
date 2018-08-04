3ac329883a2ccb7f61a18d55f390098bfa5a1ebc
Sigute <sigute.kateivaite@gmail.com>
2018-08-04 10:26:14 +0200

    Added map to activity

--- app/src/main/java/com/github/sigute/tracker/ui/fields/FieldsActivity.kt
+++ app/src/main/java/com/github/sigute/tracker/ui/fields/FieldsActivity.kt
@@ -2,28 +2,39 @@ package com.github.sigute.tracker.ui.fields
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Toast
 import com.github.sigute.tracker.R
 import com.github.sigute.tracker.api.model.Field
 import com.github.sigute.tracker.api.model.Member
 import com.github.sigute.tracker.di.DaggerWrapper
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import kotlinx.android.synthetic.main.activity_search.*
+import com.google.android.gms.maps.SupportMapFragment
+import kotlinx.android.synthetic.main.activity_fields.*
 
-class FieldsActivity : AppCompatActivity(), FieldsView {
+
+class FieldsActivity : AppCompatActivity(), FieldsView, OnMapReadyCallback {
     private val presenter by lazy { FieldsPresenter(this, DaggerWrapper(this).component.treckerService) }
+    private var map: GoogleMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+        setContentView(R.layout.activity_fields)
 
         setUpViews()
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
         presenter.retrieveData()
     }
 
-    override fun onStop() {
-        super.onStop()
-        searchButton.reset()
+    override fun onMapReady(map: GoogleMap?) {
+        this.map = map
+        //TODO
     }
 
     private fun setUpViews() {
@@ -32,15 +43,15 @@ class FieldsActivity : AppCompatActivity(), FieldsView {
 
     override fun showLoading() {
         runOnUiThread {
-            searchTerm.isEnabled = false
-            searchButton.startLoading()
+            progressBar.visibility = View.VISIBLE
+            //searchTerm.isEnabled = false
+           // searchButton.startLoading()
         }
     }
 
     override fun showFields(repositories: List<Field>) {
         runOnUiThread {
-            searchButton.loadingSuccessful()
-            searchTerm.isEnabled = true
+            //TODO stop loading
             //TODO
         }
     }
@@ -48,16 +59,14 @@ class FieldsActivity : AppCompatActivity(), FieldsView {
 
     override fun showMembers(repositories: List<Member>) {
         runOnUiThread {
-            searchButton.loadingSuccessful()
-            searchTerm.isEnabled = true
+            //TODO stop loading
             //TODO
         }
     }
 
     override fun showError(error: String) {
         runOnUiThread {
-            searchTerm.isEnabled = true
-            searchButton.loadingFailed()
+            progressBar.visibility = View.GONE
             //TODO replace this with a nicer way to show errors
             Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
         }
